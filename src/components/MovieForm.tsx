ffd77f67b9223ac6901793a5e0a8b085c0bcd269
julian <juliancancelo@gmail.com>
2025-03-20 03:55:34 +0100

    Implementado proxy en Vercel para evitar Mixed Content

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -31,7 +31,9 @@ const MovieForm: React.FC = () => {
     }
 
     try {
-      const response = await axios.post("http://jcancelo.dev/api/add_pelicula.php", formData);
+      const response = await axios.post("https://cinesur.vercel.app/api/add_pelicula.php", formData)
+      
+
       setMensaje(response.data.message || "✅ Película agregada correctamente.");
       setFormData({ titulo: "", anio: "", imagen: "", video: "", sinopsis: "" }); // Limpiar formulario
     } catch (error) {
